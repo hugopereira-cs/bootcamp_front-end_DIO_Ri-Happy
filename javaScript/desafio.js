0809a31bf4042e8e3e772ffd78c4197fe5cb28b2
hugopereira-cs <hugoper_cs@yahoo.com>
2025-09-25 17:12:03 -0300

    adiciona primeira aula e primeiro desafio da trilha de JavaScript

---
+++ javaScript/desafio.js
@@ -0,0 +1,14 @@
+const precoCombustivel = 5.79;
+const kmPorLitro = 12;
+const distanciaEmKm = 100;
+
+console.log("O valor gasto nesta viagem será R$" + ((precoCombustivel * distanciaEmKm) / kmPorLitro).toFixed(2) + ".");
+
+/* Faça um progrma para calcular o valor de uma viagem.
+
+Você terá 3 variáveis. Sendo elas:
+1 - Preço do combustível;
+2 - Gasto médio de combustível do carro por KM;
+3 - Distância em KM da viagem;
+
+*/
